4368af884d090e436c90f1064e6e731bf07f7f48
Kotaru <112825858+KotaruProject@users.noreply.github.com>
2024-03-06 04:16:23 +0400

    Create block_ips_centos.sh

---
+++ block_ips_centos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# URL файла с IP-диапазонами
+URL="https://raw.githubusercontent.com/KotaruProject/ban-ips/main/ip_ranges.txt"
+
+# Получение IP-диапазонов из файла
+IP_RANGES=$(curl -s "$URL")
+
+# Добавление каждого диапазона IP в список блокировки iptables
+for RANGE in $IP_RANGES; do
+    iptables -A INPUT -s $RANGE -j DROP
+done
+
+# Сохранение правил iptables
+iptables-save > /etc/sysconfig/iptables
+
+# Перезапуск сервиса iptables
+systemctl restart iptables
+
+echo "Готово! IP-диапазоны были добавлены в список блокировки iptables."
