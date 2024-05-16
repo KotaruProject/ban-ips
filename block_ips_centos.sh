46b3783011c14d8895f4ff9d3c16fe99b9bd7181
Kotaru <112825858+KotaruProject@users.noreply.github.com>
2024-05-16 17:11:40 +0400

    Update block_ips_centos.sh

--- block_ips_centos.sh
+++ block_ips_centos.sh
@@ -6,9 +6,20 @@ URL="https://raw.githubusercontent.com/KotaruProject/ban-ips/main/ip_ranges.txt"
 # Получение IP-диапазонов из файла
 IP_RANGES=$(curl -s "$URL")
 
-# Добавление каждого диапазона IP в список блокировки iptables
+# Функция для проверки существования правила в iptables
+rule_exists() {
+    iptables -C INPUT -s $1 -j REJECT &> /dev/null
+    return $?
+}
+
+# Добавление каждого диапазона IP в список блокировки iptables, если правило еще не существует
 for RANGE in $IP_RANGES; do
-    iptables -A INPUT -s $RANGE -j DROP
+    if rule_exists $RANGE; then
+        echo "Правило для $RANGE уже существует, пропускаем."
+    else
+        iptables -A INPUT -s $RANGE -j REJECT
+        echo "Правило для $RANGE добавлено."
+    fi
 done
 
 # Сохранение правил iptables
